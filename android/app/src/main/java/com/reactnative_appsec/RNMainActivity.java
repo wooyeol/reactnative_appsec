d9412e17b98e11b4fd64de9f3cb234ab943a8da2
rightline <intent@sk.com>
2018-10-19 14:01:13 +0900

    1. LoginActivity 추가 2. 암호화된 bundle 파일 복호화라여 로드하도록 수정 3. 결과 apk 업로드

---
+++ android/app/src/main/java/com/reactnative_appsec/RNMainActivity.java
@@ -0,0 +1,15 @@
+package com.reactnative_appsec;
+
+import com.facebook.react.ReactActivity;
+
+public class RNMainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "reactnative_appsec";
+    }
+}
